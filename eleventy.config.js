dd91b3cf253c6c9172269daf067f2d41147be31b
sylvia villegas <sylvia@svillegas.com>
2025-09-03 22:17:57 -0400

    feat: add lightningcss plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,18 @@
+import lightningCSS from "@11tyrocks/eleventy-plugin-lightningcss";
+
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("styles.css");
+
+  eleventyConfig.addPlugin(lightningCSS);
+
   return {
+    // use nunjucks everywhere
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+
     dir: {
-      input: "src"
-    }
+      input: "src",
+    },
   };
 }
